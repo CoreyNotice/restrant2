c46b1429f3b271999fe8d15115f70f64d64e1b9e
Corey N <coreynotice@yahoo.com>
2023-03-08 13:24:40 -0500

    adding boot strap grind to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -5,8 +5,9 @@ function error404() {
   return (
 
     <Def><main>
-        <h1>404:Page not Found</h1>
-        <p>Oops,sorry,we cant find this page!</p>
+
+      <img src="/images/404pic.jpg" alt="404" width="100%" height="100%" margin="auto"/>
+         
     </main>
     </Def>
   )
--- views/home.jsx
+++ views/home.jsx
@@ -1,17 +1,21 @@
-const React=require('react')
-const Def=require('./default')
+const React = require('react')
+const Def = require('./default')
 
-function home(){
-   return( <Def>
-        <main>
-            <h1>HOME</h1>
-            <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
- 
-        </main>
-    </Def>)
-
+function home () {
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div>
+                  <img src="/images/hotdog.jpg" alt="hotdog" width="25%" height="25%" margin="auto"/>
+                </div>
+             
+              <a href="/places">
+                    <button className="btn-primary">Places Page</button>
+                </a>
+            </main>
+        </Def>
+    )
 }
 
-module.exports=home
+module.exports = home
